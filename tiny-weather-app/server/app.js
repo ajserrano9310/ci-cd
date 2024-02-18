b0beb7f450055bf24cae25770c796defe8ea509f
alejandroserrano <50674421+ajserrano9310@users.noreply.github.com>
2024-02-18 13:09:10 -0700

    playing around

--- tiny-weather-app/server/app.js
+++ tiny-weather-app/server/app.js
@@ -2,17 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const student = {
+const students = [{
     name: 'Alejandro',
     lastName: 'Serrano',
-    ssn: 1231231234
-}
+    ssn: 1231231234, 
+    id: 1
+}];
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/student', (req, res) => {
+    res.send(student);
+})
+
+app.get('/student/:id', (req, res) => {
+    let student = {};
+    for(let s of students) {
+        if(s.id === req.params.id) {student = s}
+    }
+    console.log('hello')
     res.send(student);
 })
 
