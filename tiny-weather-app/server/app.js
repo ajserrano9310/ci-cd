0f72a69758b5b8f747bcb6931b987f8f1413bd76
Alejandro <50674421+ajserrano9310@users.noreply.github.com>
2024-02-19 14:53:37 -0700

    more shenaningas

--- tiny-weather-app/server/app.js
+++ tiny-weather-app/server/app.js
@@ -2,28 +2,57 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const students = [{
-    name: 'Alejandro',
-    lastName: 'Serrano',
-    ssn: 1231231234, 
-    id: 1
-}];
+const students =
+    [
+        {
+            name: 'Alejandro',
+            lastName: 'Serrano',
+            ssn: 1231231234,
+            id: 1
+        },
+        {
+            name: 'Javier',
+            lastName: 'Serrano',
+            ssn: 999999789,
+            id: 2
+        }
+    ];
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/student', (req, res) => {
-    res.send(student);
+
+    console.log('no id')
+    res.send(students);
 })
 
-app.get('/student/:id', (req, res) => {
-    let student = {};
-    for(let s of students) {
-        if(s.id === req.params.id) {student = s}
+app.get('/student/:id/', (req, res) => {
+
+    if (req.params.id) {
+        console.log("the id", req.params.id)
+        let student = students.find(s => s.id === req.params.id);
+        console.log(student)
+        if (!student) { res.send('No student found') }
+        else { res.send(student) };
+    } else {
+        res.send('No student found')
     }
-    console.log('hello')
-    res.send(student);
+
+
+    //res.send(student);
+})
+
+app.get('/superquery', (req, res) => {
+    let id = req.query.id;
+
+    if (id) {
+        res.send('Id was: ' + id);
+    }
+    else {
+        res.send('No id yet')
+    }
 })
 
 app.get('/getlist', (req, res) => {
